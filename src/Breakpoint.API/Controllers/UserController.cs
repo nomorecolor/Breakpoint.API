2b8f4310e0969fe6073a62635f534cbf8d1d87d2
Alain Elemia <alain.elemia@gmail.com>
2023-08-02 21:03:39 +0800

    Create User controller, service, repository and model (#19)
    Added User controller, service, repository and model
    Added Service and Domain installer helper static class
    
    Co-authored-by: Alain Elemia <Alain.Elemia@towerswatson.com>

---
+++ src/Breakpoint.API/Controllers/UserController.cs
@@ -0,0 +1,118 @@
+﻿using Breakpoint.Business.Services;
+using Breakpoint.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Breakpoint.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserController : ControllerBase
+	{
+		private readonly IUserService _userService;
+
+		public UserController(IUserService userService)
+		{
+			_userService = userService;
+		}
+
+		[HttpGet]
+		[ProducesResponseType(typeof(IEnumerable<UserDto>), StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<IActionResult> GetAll()
+		{
+			var users = await _userService.GetAll();
+
+			if (users == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(users);
+		}
+
+		[HttpGet("{id}")]
+		[ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<IActionResult> GetById(int id)
+		{
+			if (id <= 0)
+			{
+				return BadRequest();
+			}
+
+			var user = await _userService.GetById(id);
+
+			if (user == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(user);
+		}
+
+		[HttpPost]
+		[ProducesResponseType(typeof(UserDto), StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<IActionResult> Add(UserDto userDto)
+		{
+			// TODO: Add dto validator
+
+			await _userService.Add(userDto);
+
+			return CreatedAtAction(nameof(GetById), new { id = userDto.Id });
+		}
+
+		[HttpPut("{id}")]
+		[ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<IActionResult> Update(int id, UserDto userDto)
+		{
+			// TODO: Add dto validator
+
+			if (id <= 0)
+			{
+				return BadRequest();
+			}
+
+			var user = await _userService.GetById(id);
+
+			if (user == null)
+			{
+				return NotFound();
+			}
+
+			await _userService.Update(id, userDto);
+
+			return Ok();
+		}
+
+		[HttpDelete("{id}")]
+		[ProducesResponseType(typeof(void), StatusCodes.Status204NoContent)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		public async Task<IActionResult> Delete(int id)
+		{
+			// TODO: Add dto validator
+
+			if (id <= 0)
+			{
+				return BadRequest();
+			}
+
+			var user = await _userService.GetById(id);
+
+			if (user == null)
+			{
+				return NotFound();
+			}
+
+			await _userService.Delete(id);
+
+			return NoContent();
+		}
+	}
+}
---
+++ src/Breakpoint.Business/Services/UserService.cs
@@ -0,0 +1,87 @@
+﻿using Breakpoint.Contracts;
+using Breakpoint.Domain.Models;
+using Breakpoint.Domain.Repositories;
+
+namespace Breakpoint.Business.Services
+{
+	public interface IUserService
+	{
+		public Task<IEnumerable<UserDto>> GetAll();
+		public Task<UserDto?> GetById(int id);
+		public Task Add(UserDto userDto);
+		public Task Update(int id, UserDto userDto);
+		public Task Delete(int id);
+	}
+
+	public class UserService : IUserService
+	{
+		private readonly IUserRepository _userRepository;
+
+		public UserService(IUserRepository userRepository)
+		{
+			_userRepository = userRepository;
+		}
+
+		public async Task<IEnumerable<UserDto>> GetAll()
+		{
+			var laptops = await _userRepository.GetAll();
+
+			if (!laptops.Any())
+			{
+				return null!;
+			}
+
+			return laptops.Select(x => new UserDto
+			{
+				Id = x.Id,
+				FirstName = x.FirstName,
+				LastName = x.LastName
+			});
+		}
+
+		public async Task<UserDto?> GetById(int id)
+		{
+			var user = await _userRepository.GetById(id);
+
+			if (user == null)
+			{
+				return null;
+			}
+
+			return new UserDto
+			{
+				Id = user.Id,
+				FirstName = user.FirstName,
+				LastName = user.LastName
+			};
+		}
+
+		public async Task Add(UserDto userDto)
+		{
+			var user = new User
+			{
+				Id = userDto.Id,
+				FirstName = userDto.FirstName,
+				LastName = userDto.LastName
+			};
+
+			await _userRepository.Add(user);
+		}
+
+		public async Task Update(int id, UserDto userDto)
+		{
+			var user = new User
+			{
+				FirstName = userDto.FirstName,
+				LastName = userDto.LastName
+			};
+
+			await _userRepository.Update(id, user);
+		}
+
+		public async Task Delete(int id)
+		{
+			await _userRepository.Delete(id);
+		}
+	}
+}
